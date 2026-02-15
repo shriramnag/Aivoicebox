eb0c91f79b7c0355b78daea1b870a95eed8854d0
Shriram Nag <Shriramjdp494.0.0@gmail.com>
2026-02-15 21:07:07 +0530

    Update language_guard.py

--- language_guard.py
+++ language_guard.py
@@ -1,6 +1,5 @@
 import re
 def force_hindi_only(text):
-    # सिर्फ हिंदी वर्णमाला और विराम चिह्नों को अनुमति दें [cite: 2025-11-23]
+    # सिर्फ हिंदी और विराम चिह्नों को अनुमति दें [cite: 2025-11-23]
     allowed_pattern = r'[^\u0900-\u097F\s।,.?!]'
     return re.sub(allowed_pattern, '', text)
-  
