c7e003aac84060d757da513af0cfa22921bfbaf3
Shriram Nag <Shriramjdp494.0.0@gmail.com>
2026-02-15 21:06:16 +0530

    Update app_config.py

--- app_config.py
+++ app_config.py
@@ -1,7 +1,5 @@
-class Config:
-    MODEL_NAME = "Shriram_Voice_v2"
-    REPO_ID = "Shriramnag/My-Shriram-Voice"
-    # Ramai.pth का डायरेक्ट लिंक
-    HF_PTH = "https://huggingface.co/Shriramnag/My-Shriram-Voice/resolve/main/Ramai.pth"
-    # Index फाइल का डायरेक्ट लिंक
-    HF_INDEX = "https://huggingface.co/Shriramnag/My-Shriram-Voice/resolve/main/added_IVF759_Flat_nprobe_Ramai_Shri_Ram_Voice_Training.index"
+MODEL_CONFIG = {
+    "repo_id": "Shriramnag/My-Shriram-Voice",
+    "model_file": "Ramai.pth",
+    "index_file": "added_IVF759_Flat_nprobe_Ramai_Shri_Ram_Voice_Training.index"
+}
