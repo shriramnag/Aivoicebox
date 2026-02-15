53d5a0bb4f15592b26fc21a53f078ac8340ec313
Shriram Nag <Shriramjdp494.0.0@gmail.com>
2026-02-15 21:08:34 +0530

    Update audio_cleaner.py

--- audio_cleaner.py
+++ audio_cleaner.py
@@ -1,12 +1,11 @@
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
-def clean_stutter(audio_path):
+def clean_audio(audio_path):
     sound = AudioSegment.from_file(audio_path)
-    # हकलाने वाले साइलेंस को पहचान कर हटाना [cite: 2026-01-06]
+    # सन्नाटे को पहचान कर हटाना [cite: 2026-01-06]
     chunks = split_on_silence(sound, min_silence_len=300, silence_thresh=-45, keep_silence=100)
     combined = AudioSegment.empty()
     for chunk in chunks: combined += chunk
     combined.export(audio_path, format="wav")
     return audio_path
-  
