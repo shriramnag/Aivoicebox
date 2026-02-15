dd6cb7118e3c221f9ae3132890e39f889e6ed698
Shriram Nag <Shriramjdp494.0.0@gmail.com>
2026-02-15 21:07:53 +0530

    Update text_engine.py

--- text_engine.py
+++ text_engine.py
@@ -3,7 +3,7 @@ from language_guard import force_hindi_only
 
 def split_into_chunks(text, chunk_size=250):
     text = force_hindi_only(text)
-    # वाक्यों को पूर्ण विराम के आधार पर तोड़ें [cite: 2026-01-06]
+    # वाक्यों को '।' के आधार पर तोड़ें [cite: 2026-01-06]
     sentences = re.split(r'(?<=[।?!])\s+', text)
     chunks, current = [], ""
     for s in sentences:
